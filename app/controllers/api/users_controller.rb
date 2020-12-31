dc223f95c02c3dd983b29c3815a536fe382d5308
haileybongo <haileyebongo@gmail.com>
2020-12-30 21:45:28 -0800

    updating resources

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,11 @@
-class UsersController < ApplicationController
+class Api::UsersController < ApplicationController
 
+    def create
+        binding.pry
+        render json:  { message: 'Test' }
+    end
+
+    
     def follow
         @user = User.find(params[:id])
         current_user.followees << @user
@@ -11,4 +17,11 @@ class UsersController < ApplicationController
         current_user.followed_users.find_by(followee_id: @user.id).destroy
         redirect_back(fallback_location: user_path(@user))
       end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email)
+    end
+
 end
